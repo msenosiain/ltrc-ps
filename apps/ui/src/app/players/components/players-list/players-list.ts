0a558f277d61937be91319d26d97507a30bf66ab
Miguel Senosiain <msenosiain@gmail.com>
2025-12-06 18:06:15 -0300

    Add players routes

---
+++ apps/ui/src/app/players/components/players-list/players-list.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ltrc-players-list',
+  imports: [],
+  templateUrl: './players-list.html',
+  styleUrl: './players-list.scss',
+})
+export class PlayersListComponent {}
---
+++ apps/ui/src/app/players/players.routes.ts
@@ -0,0 +1,7 @@
+import { Routes } from '@angular/router';
+import { PlayersListComponent } from './components/players-list/players-list';
+
+
+export const PLAYERS_ROUTES: Routes = [
+  { path: '', component: PlayersListComponent },
+];
