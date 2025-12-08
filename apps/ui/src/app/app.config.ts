0dd097aa62a95d2693520c1ddd7290756148cb16
Miguel Senosiain <msenosiain@gmail.com>
2025-12-08 01:20:01 -0300

    Add navbar

--- apps/ui/src/app/app.config.ts
+++ apps/ui/src/app/app.config.ts
@@ -1,10 +1,34 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  InjectionToken,
+  Provider,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+
+export interface ApiConfig {
+  baseUrl: string;
+}
+
+export const API_CONFIG: ApiConfig = {
+  baseUrl: 'http://localhost:3000/api',
+};
+export const API_CONFIG_TOKEN = new InjectionToken<ApiConfig>(
+  'API_CONFIG_TOKEN'
+);
+
+const apiConfigProvider: Provider = {
+  provide: API_CONFIG_TOKEN,
+  useValue: API_CONFIG,
+};
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
+    provideHttpClient(withFetch()),
+    apiConfigProvider
   ],
 };
