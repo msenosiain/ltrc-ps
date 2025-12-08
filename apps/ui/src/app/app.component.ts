f11b7d0052ea9dceebf714f5b04c70b117bc8b2b
Senosiain, Miguel <miguel.senosiain@mmc.com>
2025-12-08 09:16:27 -0300

    Update title

--- apps/ui/src/app/app.component.ts
+++ apps/ui/src/app/app.component.ts
@@ -1,6 +1,13 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, inject, OnInit } from '@angular/core';
+import {
+  ActivatedRoute,
+  NavigationEnd,
+  Router,
+  RouterModule,
+} from '@angular/router';
 import { NavbarComponent } from './common/components/navbar/navbar.component';
+import { Title } from '@angular/platform-browser';
+import { filter, map, mergeMap } from 'rxjs';
 
 @Component({
   imports: [RouterModule, NavbarComponent],
@@ -8,6 +15,25 @@ import { NavbarComponent } from './common/components/navbar/navbar.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'ui';
+export class AppComponent implements OnInit {
+  private router = inject(Router);
+  private activatedRoute = inject(ActivatedRoute);
+  private titleService = inject(Title);
+
+  ngOnInit() {
+    this.router.events
+      .pipe(
+        filter((event) => event instanceof NavigationEnd),
+        map(() => this.activatedRoute),
+        map((route) => {
+          while (route.firstChild) route = route.firstChild;
+          return route;
+        }),
+        mergeMap((route) => route.data)
+      )
+      .subscribe((data) => {
+        const pageTitle = data['title'] || 'Los Tordos';
+        this.titleService.setTitle(pageTitle);
+      });
+  }
 }
