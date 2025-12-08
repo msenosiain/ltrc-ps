b6781c082eb57c501046699b5fa5506d42b737a0
Senosiain, Miguel <miguel.senosiain@mmc.com>
2025-12-08 03:17:07 -0300

    Translate positions in table

--- apps/ui/src/app/players/components/players-list/players-list.component.ts
+++ apps/ui/src/app/players/components/players-list/players-list.component.ts
@@ -1,19 +1,18 @@
 import { AfterViewInit, Component, inject, ViewChild } from '@angular/core';
-import { MatPaginator } from '@angular/material/paginator';
-import { MatSort } from '@angular/material/sort';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatSort, MatSortModule } from '@angular/material/sort';
 import { PlayersService } from '../../services/players.service';
 import { PlayersDataSource } from '../../services/players.datasource';
-import { PlayerFilters, PlayerPositionEnum } from '@ltrc-ps/shared-api-model';
+import { PlayerPositionEnum } from '@ltrc-ps/shared-api-model';
 import { CommonModule } from '@angular/common';
 import { PlayerSearchComponent } from '../player-search/player-search.component';
 import { MatTableModule } from '@angular/material/table';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatSortModule } from '@angular/material/sort';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { positionOptions } from '../../position-options';
 
 @Component({
   selector: 'ltrc-players-list',
@@ -36,7 +35,14 @@ import { MatInputModule } from '@angular/material/input';
 export class PlayersListComponent implements AfterViewInit {
   private playersService = inject(PlayersService);
 
-  displayedColumns = ['photoId', 'firstName', 'lastName', 'nickName', 'position'];
+  displayedColumns = [
+    'photoId',
+    'firstName',
+    'lastName',
+    'nickName',
+    'position',
+    'alternatePosition',
+  ];
   dataSource = new PlayersDataSource(this.playersService);
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
@@ -49,23 +55,35 @@ export class PlayersListComponent implements AfterViewInit {
     // Suscripción al sort
     this.sort.sortChange.subscribe(() => {
       this.paginator.pageIndex = 0;
-      this.dataSource.setSorting(this.sort.active, this.sort.direction as 'asc' | 'desc');
+      this.dataSource.setSorting(
+        this.sort.active,
+        this.sort.direction as 'asc' | 'desc'
+      );
     });
 
     // Suscripción a la paginación
     this.paginator.page.subscribe(() => {
-      this.dataSource.setPage(this.paginator.pageIndex, this.paginator.pageSize);
+      this.dataSource.setPage(
+        this.paginator.pageIndex,
+        this.paginator.pageSize
+      );
     });
   }
 
-  applyFilters(filters: { searchTerm?: string; position?: PlayerPositionEnum }) {
-    const playerFilters: PlayerFilters = {};
-
+  applyFilters(filters: {
+    searchTerm?: string;
+    position?: PlayerPositionEnum;
+  }) {
     this.paginator.pageIndex = 0;
     this.dataSource.setFilters(filters);
   }
 
   getPlayerPhotoUrl(playerId: string): string {
     return `http://localhost:3000/api/players/${playerId}/photo`;
+  }
+
+  getPositionLabel(position: PlayerPositionEnum): string {
+    const option = positionOptions.find(o => o.id === position);
+    return option ? option.name : position;
   }
 }
