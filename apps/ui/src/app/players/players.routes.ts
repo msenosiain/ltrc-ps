904c64ea9fa995ce0488690bec8d01a46ed5aad2
Miguel Senosiain <msenosiain@gmail.com>
2026-01-02 22:05:52 -0300

    Ad plyer detail and player edit components

--- apps/ui/src/app/players/players.routes.ts
+++ apps/ui/src/app/players/players.routes.ts
@@ -1,7 +1,22 @@
 import { Routes } from '@angular/router';
 import { PlayersListComponent } from './components/players-list/players-list.component';
-
+import { PlayerDetailComponent } from './components/player-detail/player-detail.component';
+import { PlayerEditComponent } from './components/player-edit/player-edit.component';
 
 export const PLAYERS_ROUTES: Routes = [
-  { path: '', component: PlayersListComponent, data: { title: 'Plantel - Los Tordos' }, },
+  {
+    path: '',
+    component: PlayersListComponent,
+    data: { title: 'Plantel - Los Tordos' },
+  },
+  {
+    path: ':id',
+    component: PlayerDetailComponent,
+    data: { title: 'Detalle del jugador' },
+  },
+  {
+    path: ':id/edit',
+    component: PlayerEditComponent,
+    data: { title: 'Editar jugador' },
+  },
 ];
