a84066a83658f73dfa30f7039465aaac6a90aaee
Senosiain, Miguel <miguel.senosiain@mmc.com>
2026-02-23 16:39:22 -0300

    chore(migrate): add local 'typescript-eslint' workspace adapter and enable workspaces

---
+++ packages/typescript-eslint/index.js
@@ -0,0 +1,4 @@
+// Local adapter package to satisfy require('typescript-eslint') used by @nx/eslint-plugin
+module.exports = require('../node_modules/@typescript-eslint/eslint-plugin/dist/index.js');
+module.exports.default = module.exports;
+
