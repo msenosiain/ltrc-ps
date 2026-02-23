5f1354334b5ccc1e45a3e8492f58f8a0cd4fc9a6
Senosiain, Miguel <miguel.senosiain@mmc.com>
2026-02-23 15:30:20 -0300

    fix(jest): correct root jest.preset path for libs/shared/api

--- libs/shared/api/jest.config.ts
+++ libs/shared/api/jest.config.ts
@@ -1,6 +1,6 @@
 export default {
   displayName: 'shared-api-model',
-  preset: '../jest.preset.js',
+  preset: '../../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
