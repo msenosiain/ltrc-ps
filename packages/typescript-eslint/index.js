307f1ff9ca8b46093d762972a0898ee012f6fca8
Senosiain, Miguel <miguel.senosiain@mmc.com>
2026-02-23 16:39:43 -0300

    chore(migrate): remove temporary shim from node_modules to prefer workspace package

--- packages/typescript-eslint/index.js
+++
@@ -1,4 +0,0 @@
-// Local adapter package to satisfy require('typescript-eslint') used by @nx/eslint-plugin
-module.exports = require('../node_modules/@typescript-eslint/eslint-plugin/dist/index.js');
-module.exports.default = module.exports;
-
