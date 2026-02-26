1807dda6771187fc5cf26a2492e357911c7217c3
Senosiain, Miguel <miguel.senosiain@mmc.com>
2026-02-25 21:00:18 -0300

    Update Player UI unit tests

--- apps/ui/src/app/players/components/player-form/player-form.component.spec.ts
+++ apps/ui/src/app/players/components/player-form/player-form.component.spec.ts
@@ -1,16 +1,27 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { PlayerFormComponent } from './player-form.component';
+import { PlayersService } from '../../services/players.service';
+
 describe('PlayerFormComponent', () => {
   let component: PlayerFormComponent;
   let fixture: ComponentFixture<PlayerFormComponent>;
+
+  const playersServiceMock = {
+    getPlayerPhotoUrl: jest.fn().mockReturnValue(''),
+  } as Partial<PlayersService>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [PlayerFormComponent],
+      providers: [
+        { provide: PlayersService, useValue: playersServiceMock },
+      ],
     }).compileComponents();
     fixture = TestBed.createComponent(PlayerFormComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
+
   it('should create', () => {
     expect(component).toBeTruthy();
   });
