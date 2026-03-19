8bb7403377483c264169635449a4fc7d181bd324
Miguel Senosiain <msenosiain@gmail.com>
2026-03-19 15:55:59 -0300

    Remove unused files

--- apps/ui/src/environments/environment.production.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  apiBaseUrl: 'https://ps-be.lostordos.com.ar/api/v1',
-  // Keys used to store tokens in localStorage
-  accessTokenKey: 'access_token',
-  refreshTokenKey: 'refresh_token',
-};
